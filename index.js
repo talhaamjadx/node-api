df45ce3937b628cfb60296f1d7fdc9aed5e16520
talha789 <talha.amjad@techandover.com>
2021-12-21 13:38:06 +0500

    cors headers set

--- index.js
+++ index.js
@@ -9,6 +9,13 @@ const app = express();
 
 app.use(bodyParser.json())
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*")
+    res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,PATCH")
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization")
+    next()
+})
+
 app.use('/api', apiRouter)
 
 app.listen(process.env.PORT, () => console.log("server is running on port "+process.env.PORT))
